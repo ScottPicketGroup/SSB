946dffb949a1f67b1f7b7327b5ae9023141ce106
Marek Reid <mjr@Mareks-MBP.fritz.box>
2022-04-27 06:32:36 +0200

    fixed landing renderer

--- src/components/Pages/Landing/Intro.js
+++ src/components/Pages/Landing/Intro.js
@@ -3,17 +3,22 @@ import React from "react"
 import { SectionContainer } from "../../StyledComponents/containers.css"
 import { Heading1 } from "../../StyledComponents/typography.css"
 import { IntroWrapper } from "./landingPage.css"
-import Renderer from "../../rich-text-renderers/sample"
+import Renderer from "../../rich-text-renderers/LandingQuote"
 
 const Intro = ({ quoteHeading, quoteText }) => {
   return (
-  <SectionContainer marginTop="xl" marginBottom="lg" width="50%" mbOnMob="2.5rem">
+    <SectionContainer
+      marginTop="xl"
+      marginBottom="lg"
+      width="50%"
+      mbOnMob="2.5rem"
+    >
       <IntroWrapper>
-  {quoteHeading &&   <Heading1 marginBottom="md">{quoteHeading}</Heading1>}
-  {quoteText &&      <Renderer node={quoteText} />}
+        {quoteHeading && <Heading1 marginBottom="md">{quoteHeading}</Heading1>}
+        {quoteText && <Renderer node={quoteText} />}
       </IntroWrapper>
-    </SectionContainer> 
-  ) 
+    </SectionContainer>
+  )
 }
 
 export default Intro
--- src/components/rich-text-renderers/LandingQuote.js
+++ src/components/rich-text-renderers/LandingQuote.js
@@ -14,9 +14,6 @@ const Renderer = ({ node }) => {
       [BLOCKS.HEADING_1]: (node, children) => <Heading1 color="black">{children}</Heading1>,
       [BLOCKS.HEADING_2]: (node, children) => <Heading2 color="black">{children}</Heading2>,
       [BLOCKS.HEADING_3]: (node, children) => <Heading3 color="black">{children}</Heading3>,
-      [BLOCKS.HYPERLINK]: (node, children) => (
-        <BC1 marginBottom="sm" style={{color: `red !important`}}>{children}</BC1>
-      ),
       [BLOCKS.PARAGRAPH]: (node, children) => (
         <BC1 marginBottom="sm" color="black">{children}</BC1>
       ),
